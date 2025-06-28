ed22f9769eaa096444691a4660fd5d5d7299bbda
jaqjajaq <spherical.bruh@gmail.com>
2025-06-29 02:52:57 +0800

    Add auto dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+//import { RouterLink, RouterView } from 'vue-router'
+import { useColorMode } from '@vueuse/core';
+
+const mode = useColorMode();
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="p-4 bg-muted">
+    HA HA HA HA
+  </div>
 </template>
 
 <style scoped>
